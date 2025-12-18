c1f6dd7e1f30304084b021ce5cbad5b727200584
Bohdan <65454061+clswhre@users.noreply.github.com>
2025-12-18 16:04:12 +0200

    Add files via upload

--- fish/config.fish
+++ fish/config.fish
@@ -7,7 +7,3 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 #end
 
 fish_add_path /home/clswhre/.spicetify
-
-function ls --wraps='eza -al --color=always --group-directories-first --icons --no-permissions --no-user' --description 'alias ls=eza (long, no perms, no user)'
-    eza -al --color=always --group-directories-first --icons --no-permissions --no-user $argv
-end
