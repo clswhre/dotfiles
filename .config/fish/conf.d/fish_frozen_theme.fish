060c9c366f4db02eb9e249805df379f41f7c3df7
Bohdan Kryzhov <krugov1bogdan@gmail.com>
2026-01-20 15:26:19 +0200

    ///

--- .config/fish/conf.d/fish_frozen_theme.fish
+++ .config/fish/conf.d/fish_frozen_theme.fish
@@ -5,40 +5,40 @@
 #     man fish-interactive | less +/^SYNTAX.HIGHLIGHTING
 # for appropriate commands to add to ~/.config/fish/config.fish instead.
 
-set --global fish_color_autosuggestion 006363
+set --global fish_color_autosuggestion 4c566a
 set --global fish_color_cancel --reverse
-set --global fish_color_command 009999
-set --global fish_color_comment FF9640
-set --global fish_color_cwd green
-set --global fish_color_cwd_root red
-set --global fish_color_end FFB273
-set --global fish_color_error FF7400
-set --global fish_color_escape 00a6b2
-set --global fish_color_history_current --bold
-set --global fish_color_host normal
-set --global fish_color_host_remote yellow
-set --global fish_color_keyword 
+set --global fish_color_command 88c0d0
+set --global fish_color_comment 4c566a --italics
+set --global fish_color_cwd 5e81ac
+set --global fish_color_cwd_root bf616a
+set --global fish_color_end 81a1c1
+set --global fish_color_error bf616a
+set --global fish_color_escape ebcb8b
+set --global fish_color_history_current e5e9f0 --bold
+set --global fish_color_host a3be8c
+set --global fish_color_host_remote ebcb8b
+set --global fish_color_keyword 81a1c1
 set --global fish_color_normal normal
-set --global fish_color_operator 00a6b2
-set --global fish_color_option 
-set --global fish_color_param 33CCCC
-set --global fish_color_quote 5CCCCC
-set --global fish_color_redirection BF7130
-set --global fish_color_search_match white --bold --background=brblack
-set --global fish_color_selection white --bold --background=brblack
-set --global fish_color_status red
-set --global fish_color_user brgreen
+set --global fish_color_operator 81a1c1
+set --global fish_color_option 8fbcbb
+set --global fish_color_param d8dee9
+set --global fish_color_quote a3be8c
+set --global fish_color_redirection b48ead --bold
+set --global fish_color_search_match --bold --background=434c5e
+set --global fish_color_selection d8dee9 --bold --background=434c5e
+set --global fish_color_status bf616a
+set --global fish_color_user a3be8c
 set --global fish_color_valid_path --underline=single
 set --global fish_pager_color_background 
-set --global fish_pager_color_completion normal
-set --global fish_pager_color_description B3A06D
+set --global fish_pager_color_completion e5e9f0
+set --global fish_pager_color_description ebcb8b --italics
 set --global fish_pager_color_prefix normal --bold --underline=single
-set --global fish_pager_color_progress brwhite --bold --background=cyan
+set --global fish_pager_color_progress 3b4252 --bold --background=d08770
 set --global fish_pager_color_secondary_background 
 set --global fish_pager_color_secondary_completion 
 set --global fish_pager_color_secondary_description 
 set --global fish_pager_color_secondary_prefix 
-set --global fish_pager_color_selected_background --background=brblack
+set --global fish_pager_color_selected_background --background=434c5e
 set --global fish_pager_color_selected_completion 
 set --global fish_pager_color_selected_description 
 set --global fish_pager_color_selected_prefix 
